d29a1ba2fb3e62544aa649dd2b45388ac9733c47
joriscai <516003559@qq.com>
2020-03-11 14:28:43 +0800

    [feat]: add copy text in weread get catalog

--- weread/getCatalog.js
+++ weread/getCatalog.js
@@ -2,23 +2,56 @@
 // @name         Weread Get Catalog
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  try to take over the world!
+// @description  Get Catalog
 // @author       Joris Cai
 // @match        https://weread.qq.com/web/reader/*
 // @require      http://cdn.bootcss.com/jquery/1.8.3/jquery.min.js
+// @require      https://cdn.bootcss.com/clipboard.js/2.0.6/clipboard.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    const list = document.querySelectorAll('li.chapterItem');
 
-    const chapter = Array.from(list);
-    const start = chapter.findIndex(ele => Array.from(ele.classList).includes('chapterItem_current'));
+    const body = $('body');
+    const btn = $('<button id="get-catalog">获取目录</button>');
+    btn.css({
+        position: 'absolute',
+        right: 0,
+    })
+    body.prepend(btn);
+    body.on('click', '#get-catalog', (e) => {
+        const pre = prompt('MarkDown首级标题', '##');
+        start(pre, e);
+    });
 
-    const end = list.length;
-    const target = chapter.slice(start, end);
-    console.log(getMd(target));
+    function start(pre, event) {
+        const list = document.querySelectorAll('li.chapterItem');
+        const chapter = Array.from(list);
+        const start = chapter.findIndex(ele => Array.from(ele.classList).includes('chapterItem_current'));
+        const end = list.length;
+        const target = chapter.slice(start, end);
 
+        const mdText = getMd(target, pre);
+        $(event.target).attr('data-clipboard-text', mdText);
+        console.log('md: ', mdText);
+
+        const clipboard = new ClipboardJS('#get-catalog');
+        clipboard.on('success', (e) => {
+            alert('复制成功');
+            console.log('复制成功');
+            clipboard.destroy();
+        });
+        clipboard.on('error', (e) => {
+            alert('复制失败');
+            console.log('复制失败，请再次尝试');
+            clipboard.destroy();
+        });
+        // 触发执行复制
+        const newEv = Object.assign(event, {
+            delegateTarget: null,
+        });
+        clipboard.onClick(newEv);
+    }
 
     function getMd(list = [], pre = '##') {
         const arr = [];
@@ -28,9 +61,9 @@
             const classList = Array.from(a.classList);
             let prefix = pre;
             if (classList.includes('chapterItem_level2')) {
-                prefix += '#'
+                prefix += '#';
             }
-            arr.push(`${prefix} ${text}`)
+            arr.push(`${prefix} ${text}`);
         });
         return arr.join('\n');
     }
